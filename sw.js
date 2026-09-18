8a8a9f2071858cc57988c675e8bd4c224674d3bc
PAVEL <108974875+pkhlystunov@users.noreply.github.com>
2026-09-18 14:29:35 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-const CACHE_NAME = 'otipb-v2';
+const CACHE_NAME = 'otipb-v3';
 
-// Список файлов, которые жестко сохраняются в память устройства
 const ASSETS = [
   './',
   './index.html',
   './app.js',
   './style.css',
-  './manifest.json',
-  'https://githack.com'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
@@ -19,14 +17,11 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  // Очищаем старый кэш v1, чтобы принудительно обновить библиотеку PDF
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       );
     })
@@ -34,9 +29,7 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.request.url.includes('://google.com')) {
-    return;
-  }
+  if (e.request.url.includes('://google.com')) return;
   e.respondWith(
     caches.match(e.request).then((cachedResponse) => {
       return cachedResponse || fetch(e.request);
